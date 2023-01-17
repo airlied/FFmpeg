e8bcac00a53c89f1faa583edb9148336d3f0fb10
Lynne <dev@lynne.ee>
2023-01-17 05:07:53 +0100

    av1dec: add hwaccel_params_buf

--- libavcodec/av1dec.c
+++ libavcodec/av1dec.c
@@ -1171,6 +1171,7 @@ static int av1_decode_frame(AVCodecContext *avctx, AVFrame *frame,
         switch (unit->type) {
         case AV1_OBU_SEQUENCE_HEADER:
             av_buffer_unref(&s->seq_ref);
+            av_buffer_unref(&s->hwaccel_params_buf);
             s->seq_ref = av_buffer_ref(unit->content_ref);
             if (!s->seq_ref) {
                 ret = AVERROR(ENOMEM);
@@ -1410,6 +1411,7 @@ static void av1_decode_flush(AVCodecContext *avctx)
 
     ff_cbs_flush(s->cbc);
 
+    av_buffer_unref(&s->hwaccel_params_buf);
     if (avctx->hwaccel && avctx->hwaccel->flush)
         avctx->hwaccel->flush(avctx);
 }
